94d0d40271e4558f9aa6f93a199dfc8a9950eea6
Christoph 'knurd' Morrison <git@christoph-morrison.de>
2023-04-21 21:56:23 +0200

    Second test

--- src/Maschinengeist/Core/Helper.php
+++ src/Maschinengeist/Core/Helper.php
@@ -1,7 +1,24 @@
 <?php
 
-namespace \Maschinengeist\Core;
+namespace Maschinengeist\Core;
 
 class Helper {
 
+    public static function flattenArrayToMqttTopics(array $inputValues, string $mqttBaseTopic, array &$result) : bool {
+        foreach ($inputValues as $key => $value) {
+
+            if (is_array($value)) {
+                self::flattenArrayToMqttTopics($value, "$mqttBaseTopic/$key", $result);
+                continue;
+            }
+
+            # convert php internal types to strings
+            if ($value === true)    { $value = 'true';  }
+            if ($value === false)   { $value = 'false'; }
+
+            $result["$mqttBaseTopic/$key"] = $value;
+        }
+
+        return true;
+    }
 }
